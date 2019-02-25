38db2c1805ed42951f1e4cdde65854b6781e81b9
marween <annemagali.saintgeorges@gmail.com>
2019-02-25 16:00:54 +0100

    debut de travail

---
+++ app/Controllers/ArticlesController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controllers;
+
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+class CommentairesController extends Controller {
+
+	public function ...(Request $request, Response $response){
+		
+	}
+
+
+	
+
+}
