5ac2144691be53248b56f21e246dfe3560648544
marween <annemagali.saintgeorges@gmail.com>
2019-03-12 15:38:43 +0100

    display catégorie admin et home

--- app/Controllers/AdminArticlesController.php
+++ app/Controllers/AdminArticlesController.php
@@ -20,8 +20,25 @@ class AdminArticlesController extends Controller {
 			FROM articles
 			INNER JOIN users on articles.author=users.id')->fetchAll();
 
-		
-		
+		$categories = $this->container->db->query('
+			SELECT article as article_id, name
+			FROM categoriesarticles
+			iNNER JOIN categories on categorie = categories.id')->fetchAll();
+
+		foreach ($articles as &$article) {
+			# crée une boite vide pour y mettre les categories de mes articles 
+			$article['categories'] = array();
+		}
+
+		#pour chaque catégorie, on verifie sir l'id de l'article est le meme et on le joint
+		#& pour ne pas travailler sur une copie de l'article
+		foreach ($categories as $categorie) {
+			foreach ($articles as &$article) {
+				if($article['id'] === $categorie['article_id']){
+					array_push($article['categories'], $categorie['name']);
+				}
+			}
+		}
 		$args['articles'] = $articles;
 		
 		$this->render($response,'admin/ArticlesAdmin.twig', $args);
--- app/Controllers/PagesController.php
+++ app/Controllers/PagesController.php
@@ -11,18 +11,39 @@ class PagesController extends Controller {
 
 	public function home(Request $request, Response $response){
 
-		$article = $this->container->db->query('
-			SELECT 
-			articles.id, 
-			title, 
-			text, 
-			date, 
-			username as author 
-			FROM articles 
+		$articles = $this->container->db->query('
+			SELECT
+				articles.title as title,
+				articles.id as id,
+				articles.date as date,
+				articles.text as text,
+				users.username as author
+				
+			FROM articles
 			INNER JOIN users on articles.author=users.id')->fetchAll();
-		$args['articles'] = $article;
 
-		$this->render($response,'pages/home.twig', $args);
+		$categories = $this->container->db->query('
+			SELECT article as article_id, name
+			FROM categoriesarticles
+			iNNER JOIN categories on categorie = categories.id')->fetchAll();
+
+		foreach ($articles as &$article) {
+			# crée une boite vide pour y mettre les categories de mes articles 
+			$article['categories'] = array();
+		}
+
+		#pour chaque catégorie, on verifie sir l'id de l'article est le meme et on le joint
+		#& pour ne pas travailler sur une copie de l'article
+		foreach ($categories as $categorie) {
+			foreach ($articles as &$article) {
+				if($article['id'] === $categorie['article_id']){
+					array_push($article['categories'], $categorie['name']);
+				}
+			}
+		}
+		$args['articles'] = $articles;
+		
+		$this->render($response,'admin/ArticlesAdmin.twig', $args);
+
 	}
-
 }
